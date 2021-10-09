c8703a2b2be1dd47ec69a8c5d522ee191e35a79b
Marcdergute <38373817+Marcdergute@users.noreply.github.com>
2021-10-09 15:47:47 +0200

    Login

--- server/Auth.py
+++ server/Auth.py
@@ -1,7 +1,32 @@
 from flask import Blueprint
+import flask_login
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/account")
-def accountList():
-    return "list of accounts"
+
+# @auth.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if flask.request.method == 'GET':
+#         return '''
+#                <form action='login' method='POST'>
+#                 <input type='text' name='email' id='email' placeholder='email'/>
+#                 <input type='password' name='password' id='password' placeholder='password'/>
+#                 <input type='submit' name='submit'/>
+#                </form>
+#                '''
+
+#     email = flask.request.form['email']
+#     if flask.request.form['password'] == users[email]['password']:
+#         user = User()
+#         user.id = email
+#         flask_login.login_user(user)
+#         return flask.redirect(flask.url_for('protected'))
+
+#     return 'Bad login'
+
+
+# @auth.route('/protected')
+# @flask_login.login_required
+# def protected():
+#     return 'Logged in as: ' + flask_login.current_user.id
