697709896bd5d0c96a1432f5e5dc59f902441e78
Luke Zielke <neongelb99@gmail.com>
2021-10-10 12:20:38 +0200

    many things

--- addon/newIdentity.js
+++ addon/newIdentity.js
@@ -9,7 +9,11 @@ if (resultToken == null) {
 function newIdentity() {
     fetch(`http://127.0.0.1:5000/${resultToken}/new`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data)
+            chrome.storage.local.set({id: data.id});
+            fillIn(data);
+        });
 }
 
 document.addEventListener("DOMContentLoaded", initializeListener);
@@ -18,6 +22,16 @@ function initializeListener() {
     console.log("fetchData initialize")
     var button = document.getElementById("newID");
     button.addEventListener("click", newIdentity);
+}
+
+function fillIn(jsonData) {
+    document.getElementById("fakename").value = jsonData["name"];
+    document.getElementById("birthday").value = jsonData["Geburtsdatum"];
+    document.getElementById("gender").value = jsonData["Geschlecht"];
+    document.getElementById("address").value = jsonData["Addresse"];
+    document.getElementById("email").value = jsonData["Email"];
+    document.getElementById("phone").value = jsonData["Telefon"];
+    document.getElementById("lol").value = jsonData["Email_Webaddresse"];
 }
 
 })
